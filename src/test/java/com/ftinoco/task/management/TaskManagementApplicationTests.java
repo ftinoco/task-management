31cfc9dc7807d76d1876fa47558d245d12a49c11
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2024-12-23 09:03:51 -0600

    Initial commit

---
+++ src/test/java/com/ftinoco/task/management/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftinoco.task.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
