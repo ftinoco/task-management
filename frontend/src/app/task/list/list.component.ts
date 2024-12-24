1787e271341ecdd11c255c5a28566dbfc94f63a2
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2024-12-24 12:33:12 -0600

    Adding list functionality

--- frontend/src/app/task/list/list.component.ts
+++ frontend/src/app/task/list/list.component.ts
@@ -1,25 +1,46 @@
 import { Component } from '@angular/core';
 import { Task } from '../task';
 import { TaskService } from '../task.service';
-import { MatTableModule } from '@angular/material/table'; 
+import { MatTableModule } from '@angular/material/table';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
-  imports: [MatTableModule],
+  imports: [
+    MatTableModule, MatButton,
+    MatCard, MatCardContent, MatCardHeader,
+    MatCardSubtitle, MatCardTitle, MatIcon,
+    CommonModule
+  ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
   tasks: Task[] = [];
-  displayedColumns: string[] = ['id', 'title', 'description', 'dueDate', 'status'];
+  displayedColumns: string[] = ['id', 'title', 'description', 'dueDate', 'status', 'actions'];
 
   constructor(public taskService: TaskService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.taskService.getAll().subscribe((data: Task[]) => {
       this.tasks = data;
       console.log(data);
     })
+  }
+
+  addTask(): void {
+
+  }
+  
+  editTask(id: number): void {
+    console.log(id);
+  } 
+
+  deleteTask(id: number): void {
+    console.log(id);
   }
 }
